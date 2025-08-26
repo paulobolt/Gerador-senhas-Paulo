6a2309d72dce222eab8b74a2330cccdd5949ac1c
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2025-08-26 08:06:33 -0300

    26

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-const numeroSenhas = document.querySelector
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+if(tamanhoSenha > 1){
+tamanhoSenha = tamanhoSenha - 1;
+}
+numeroSenha.textContent=tamanhoSenha;
+}
+
+function aumentaTamanho(){
+    if(tamanhoSenha < 20){
+        tamanhoSenha = tamanhoSenha + 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
