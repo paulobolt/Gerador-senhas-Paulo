4099773dab2fbeade0e423e2693b1ad1bae5a57e
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2025-09-16 11:43:37 -0300

    fkj

--- main.js
+++ main.js
@@ -68,13 +68,19 @@ classificaSenha();
 }
 
 function classificaSenha() {
+
+    let entropia = tamanhoSenha*Math.log2(tamanhoSenha);
+
 forcaSenha.classList.remove('fraca', 'media', 'forte');
-if (tamanhoSenha > 11) {
+if (entropia > 57) {
 forcaSenha.classList.add('forte');
-} else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+} else if (entropia > 35 && entropia < 57) {
 forcaSenha.classList.add('media');
-} else if (tamanhoSenha <= 5) {
+} else if (entropia <= 35) {
 forcaSenha.classList.add('fraca');
 }
+
+const valorEntropia  = document.querySelector('.entropia');
+valorEntropia.textContent = "Um computador pode levar até"
 
 }
